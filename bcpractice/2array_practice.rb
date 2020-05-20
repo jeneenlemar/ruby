db29a5b9fb70814a50bd39f5c0f4abede6c83db0
Jeneen <jeneen.lemar@gmail.com>
2020-05-20 18:57:25 -0400

    old uncommitted practice

---
+++ bcpractice/2array_practice.rb
@@ -0,0 +1,4 @@
+puts "Enter a word below"
+word = gets.chomp
+word_split = word.split('')
+puts "You entered -  #{word_split}"
---
+++ bcpractice/array_practice.rb
@@ -0,0 +1,8 @@
+array = [2, 10, "a", 12, "b"]
+array << "c"
+array[3] = 7
+p array
+puts array
+
+a = 1
+puts a
